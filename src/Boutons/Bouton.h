721f315482e68b2c6ae7bec2c82811b2d6f58a4e
nzotrn <167066188+nzotrn@users.noreply.github.com>
2024-04-26 09:58:56 +0200

    Ajout Ecran, Boutons & Automate

---
+++ src/Boutons/Bouton.h
@@ -0,0 +1,13 @@
+#ifndef BOUTON_H
+#define BOUTON_H
+
+#define buttonPin A3 // Broche pour les boutons
+
+class Bouton
+{
+private:
+
+public:
+    int readButton(); // Déclaration de la fonction
+};
+#endif
