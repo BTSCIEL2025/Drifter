b1c01df138768d9e26444dfed53b287807fd9088
Leane4123 <leanemougin@gmail.com>
2024-04-26 15:20:47 +0200

    Maj gyro

--- src/TestGyro/Gyro.h
+++ src/TestGyro/Gyro.h
@@ -1,21 +1,44 @@
 #ifndef GYRO_H
 #define GYRO_H
+
+
+#include <Adafruit_PWMServoDriver.h>    // pour module PCA9586 16 chanel 12 bits (violet)
+#define PIN_LED 15
+
 class Gyro
 {
 	public :
+
+    //Constructeurs
 		gyro();
-		gyro(int dOn, int dOff);
-		void marche();
-		void arret();
+		gyro(int, int,Adafruit_PWMServoDriver*);
+
+    //Getteurs
 		bool getEtat();
 		int getdOn();
     int getdOff();
+    long getDateDebut();
+    bool getEtatLampe();
+
+    //Setters 
+    void setEtat(bool);
     void setdOn(int);
     void setdOff(int);
+    void setDateDebut(long);
+    void setEtatLampe(bool);
+
+    //Autres fonctions
+		void miseOn();
+		void miseOff();
+    void maj();
 
 	private :
-		int dOn;
-		int dOff;
-		bool etat; // 1 = On --- 0 = Off
+		int dOn;                    //durée led allumée
+		int dOff;                   //durée led eteinte
+		bool etat;                  // etat du module
+    long dateDebut;             //en ms
+    bool etatLampe;             //etat de la led
+    Adafruit_PWMServoDriver *pwm; //variable nécésaire au servo moteur
+
 };
 #endif 
